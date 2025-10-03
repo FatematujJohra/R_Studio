ef2c292f15acf4b97cc6bc8b266cf47d98051c1e
Fatema Tuj Johra <johrafatema73@gmail.com>
2025-10-03 06:16:18 +0000

    Add files via upload

---
+++ R_Data_Struc.r
@@ -0,0 +1,17 @@
+#data structure in R
+#vector
+v <- c(1, 2, 3, 4, 5)
+print(v)
+#list
+l <- list(name="John", age=30, is_student=FALSE)
+print(l)
+#matrix
+m <- matrix(1:9, nrow=3, ncol=3)
+print(m)
+#array
+a <- array(1:12, dim=c(3,4))
+print(a)
+#data frame
+df <- data.frame(Name=c("Alice", "Bob", "Charlie"), Age=c(25
+, 30, 35))
+print(df)
