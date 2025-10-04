27568fb28e87d271758db428f9be7ec23a7fd025
Fatema Tuj Johra <johrafatema73@gmail.com>
2025-10-04 18:22:09 +0000

    max_min in R

---
+++ R_Max_Min.r
@@ -0,0 +1,14 @@
+#largest
+#Data_Cars<-mtcars
+#print(max(Data_Cars$hp))
+#smallest
+#print(min(Data_Cars$hp))
+#find the index position
+#print(which.max(Data_Cars$hp))
+#print(which.min(Data_Cars$hp))
+Data_Cars<-mtcars
+rownames(Data_Cars)
+#print[which.max(Data_Cars$hp)]
+rownames(Data_Cars)
+#print[which.min(Data_Cars$hp)]
+
